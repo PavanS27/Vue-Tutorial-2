c8cf80b43c450bcffd542cec3468ab8601114c59
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-09-07 15:10:34 +0530

    events

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ new Vue({
     name: "Paan",
     website: "https://www.google.com/",
     websiteTag: "<a href='https://www.google.com/'>Google</a>",
+    age: 20,
+    x: 0,
+    y: 0,
   },
   methods: {
     greet: function (time) {
       return "Good" + " " + time + " " + this.name;
+    },
+    add: function (inc) {
+      this.age += inc;
+    },
+    subtract: function (dec) {
+      this.age -= dec;
+    },
+    updateXY: function (event) {
+      (this.x = event.offsetX), (this.y = event.offsetY);
     },
   },
 });
