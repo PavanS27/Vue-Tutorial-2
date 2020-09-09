57e3d3e4223cf2b51649e3ab6e59342d017da287
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-09-09 12:56:28 +0530

    input binding

--- vueplaylist/src/App.vue
+++ vueplaylist/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+    <!-- <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
     <paans v-bind:ninjas="ninjas"></paans>
     <app-footer v-bind:title="title"></app-footer>
     <form-helper>
@@ -15,41 +15,44 @@
       <div slot="form-controls">
         <button v-on:click="handleSubmit">Submit</button>
       </div>
-    </form-helper>
+    </form-helper>-->
+    <blog></blog>
   </div>
 </template>
 
 <script>
-import Header from "./components/header";
-import Footer from "./components/footer";
-import Paans from "./components/Paans";
-import formHelper from "./components/formHelper";
+// import Header from "./components/header";
+// import Footer from "./components/footer";
+// import Paans from "./components/Paans";
+// import formHelper from "./components/formHelper";
+import Blog from "./components/Blog";
 
 export default {
   components: {
-    "app-header": Header,
-    "app-footer": Footer,
-    paans: Paans,
-    "form-helper": formHelper,
+    //   "app-header": Header,
+    //   "app-footer": Footer,
+    //   paans: Paans,
+    //   "form-helper": formHelper,
+    blog: Blog,
   },
 
   data() {
     return {
-      ninjas: [
-        { name: "Ryu", speciality: "Vue Components", show: false },
-        { name: "Crystal", speciality: "HTML Wizardry", show: false },
-        { name: "Hitoshi", speciality: "Click Events", show: false },
-        { name: "Tango", speciality: "Conditionals", show: false },
-        { name: "Kami", speciality: "Webpack", show: false },
-        { name: "Yoshi", speciality: "Data Diggin", show: false },
-      ],
-      title: "Welcome paan",
+      //   ninjas: [
+      //     { name: "Ryu", speciality: "Vue Components", show: false },
+      //     { name: "Crystal", speciality: "HTML Wizardry", show: false },
+      //     { name: "Hitoshi", speciality: "Click Events", show: false },
+      //     { name: "Tango", speciality: "Conditionals", show: false },
+      //     { name: "Kami", speciality: "Webpack", show: false },
+      //     { name: "Yoshi", speciality: "Data Diggin", show: false },
+      //   ],
+      //   title: "Welcome paan",
     };
   },
   methods: {
-    updateTitle: function (event) {
-      this.title = event;
-    },
+    // updateTitle: function (event) {
+    //   this.title = event;
+    // },
   },
 };
 </script>
