3c4bd946bb5de054c5ba386b3d0e0e8999f6eebe
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-09-09 12:06:36 +0530

    event bus

--- vueplaylist/src/components/footer.vue
+++ vueplaylist/src/components/footer.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import { bus } from "../main";
 export default {
   props: {
     title: {
@@ -15,6 +16,11 @@ export default {
     return {
       copyright: "Copyright 2020 Vue Paans",
     };
+  },
+  created() {
+    bus.$on("titleChanged", (data) => {
+      this.title = data;
+    });
   },
 };
 </script>
--- vueplaylist/src/components/header.vue
+++ vueplaylist/src/components/header.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import { bus } from "../main";
 export default {
   props: {
     title: {
@@ -18,7 +19,9 @@ export default {
   },
   methods: {
     changeTitle: function () {
-      this.$emit("changeTitle", "Vue mamas");
+      // this.$emit("changeTitle", "Vue mamas");
+      this.title = "Vue Wizards";
+      bus.$emit("titleChanged", "Vue Memes");
     },
   },
 };
